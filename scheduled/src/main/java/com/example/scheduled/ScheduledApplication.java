1e458c4befccffbce1c6b09afc9b8cca7f86f506
csj <tianyuan881214@sina.com>
2018-04-20 14:13:02 +0800

    Initial commit

---
+++ scheduled/src/main/java/com/example/scheduled/ScheduledApplication.java
@@ -0,0 +1,12 @@
+package com.example.scheduled;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduledApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScheduledApplication.class, args);
+	}
+}
